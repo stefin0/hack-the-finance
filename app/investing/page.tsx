2a685aef3607c4b631ee2ffe9e26f645fc2fd9af
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-03-04 12:51:33 -0800

    yk the vibes

--- app/investing/page.tsx
+++ app/investing/page.tsx
@@ -4,7 +4,7 @@ export default function investing() {
   return (
     <>
       <h1 className="text-center mt-8 font-extrabold">
-        This is the Investing page.
+        This is the Investing page by marco.
       </h1>
       <div className="flex justify-center">
         <Link href="" className="bg-slate-700 p-3 rounded mx-2">
