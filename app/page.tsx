8b0cf2d8f12fe20800c4c740f6dd60e34701024a
stefin <stefinracho@gmail.com>
2023-03-03 19:16:46 -0800

    hacking

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <h1 className="text-center mt-8 font-extrabold">Hack The Finance</h1>
+      <h1 className="text-center mt-8 font-extrabold">
+        Hack The Finance...Hacking...
+      </h1>
     </main>
   );
 }
